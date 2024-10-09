c798e531408effd5e0d209350586da8981275812
Jonathan Laperle <jonathan.laperle@usherbrooke.ca>
2024-10-09 12:22:31 -0400

    don't remove double slashes from protocols

--- client/src/utils/redirect.test.ts
+++ client/src/utils/redirect.test.ts
@@ -10,6 +10,10 @@ test("route prefix changes", async () => {
     expect(withPrefix("http://")).toEqual("http://");
     expect(withPrefix("/")).toEqual("/prefix/");
     expect(withPrefix("/home")).toEqual("/prefix/home");
+    // should preserve urls in query params
+    expect(withPrefix("/authnz/cilogon/login/?idphint=https://test.com")).toEqual(
+        "/prefix/authnz/cilogon/login/?idphint=https://test.com"
+    );
     // ensure that it can only be called once
     expect(withPrefix(withPrefix("/home"))).toEqual("/prefix/prefix/home");
     // This doesn't do what it looks like it should do?
